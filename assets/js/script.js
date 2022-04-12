f494b8bc45a6817c3a19983197811d0cfe0d6203
Erik Dell <edellenator@gmail.com>
2022-04-11 22:38:22 -0700

    tasks can be edited and saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const textBlockEl = document.querySelectorAll(".text");
-const saveBtnEl = document.querySelector(".saveBtn");
+const saveBtnEl = document.querySelectorAll(".saveBtn");
 const currentTime = document.getElementById("currentDay");
 const hourText = document.querySelectorAll(".hourtext");
+const selectTextBox = document.querySelector('.text');
 
+var textArray = [
+];
 
 var headerClock = function () {
     var now = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -13,11 +16,8 @@ setInterval(headerClock, 1000);
 headerClock();
 
 var setHourText = function () {
-    // var setHour = 9;
-    //     var setTime = moment().set('hour', setHour).set('second', 0).format('h a')
-    //     console.log(setTime)
     for (var i = 0; i < hourText.length; i++) {
-        var setHour = 21 + parseInt([i]);
+        var setHour = 9 + parseInt([i]);
         // console.log(setHour);
         var setTime = moment().set('hour', setHour).set('second', 0).format('h a');
         // console.log(setTime)
@@ -30,39 +30,82 @@ var setHourText = function () {
 setHourText();
 
 
+// set text block color based on time in 
 
-
- var textBlockColor = function () {
-     //  var setHour = moment().hour(parseHour);
-     //  console.log(setHour);
-    //  var hour = moment().hour();
-    //  console.log(hour);
-     
-    //  console.log(parseHour);
-    //  console.log(hourPlusOne);
+var textBlockColor = function () {
      for (var i = 0; i < hourText.length; i++){
+        var now = moment()
+        var timeBlock = moment().hour(parseHour)
         var parseHour = parseInt(moment(hourText[i].textContent, ['h a']).format('H'));
-           if (moment().format('h a') === hourText[i].textContent) {
+           if (moment(timeBlock).isSame(now)) {
                textBlockEl[i].classList += " present";
                textBlockEl[i].classList.remove("past", "future");
            }
-           else if (moment().hour() < moment().hour(parseHour)) {
-            textBlockEl[i].classList += " future";
-            textBlockEl[i].classList.remove("past", "present");
-           }
-
-           else if (moment().hour() > moment().hour(parseHour)) {
+           else if (moment(timeBlock).isBefore(now)) {
             textBlockEl[i].classList += " past";
             textBlockEl[i].classList.remove("future", "present");
            }
-    
-    }
-    
-     
 
+           else if (moment(timeBlock).isAfter(now)) {
+            textBlockEl[i].classList += " future";
+            textBlockEl[i].classList.remove("past", "present");
+           };
+    
+    };   
+};
+
+//check each minute if the time block color should be updated
+setInterval(textBlockColor(),1000 * 60);
+
+var textBlockEdit = function (index) {
+    return function() {
+        console.log('you clicked on text area' + index);
+        textBlockEl[index].setAttribute('contenteditable', 'true');
+    }
+};
+
+for (var i = 0; i < textBlockEl.length; i++) {
+    textBlockEl[i].addEventListener('click', textBlockEdit(i))
+};
+      
+var textBlockSave = function (index) {
+    return function() {
+        console.log('you clicked button #' + index)
+        if (textBlockEl[index].getAttribute('contenteditable')){
+            textBlockEl[index].removeAttribute('contenteditable')
+        }
+        var updatedTextArray = []
+
+        for (var i = 0; i < textBlockEl.length; i++) {
+            updatedTextArray.push(textBlockEl[i].textContent)
+        }
+        textArray = updatedTextArray
+
+        setStorage()
+    }
 }
 
-textBlockColor()
+var setStorage = function () {
+        localStorage.setItem('timeblocktext', JSON.stringify(textArray));
+};
 
+var getStorage = function() {
+    console.log('the window is loaded')
+    var retrievedText = localStorage.getItem('timeblocktext')
+    if(!retrievedText) {
+        return false
+    };
+    console.log(retrievedText);
+    retrievedText = JSON.parse(retrievedText);
+    console.log(retrievedText);
 
+    for (var i = 0; i < textBlockEl.length; i++) {
+        textBlockEl[i].textContent = retrievedText[i];
+    }
+}
 
+for (var i = 0; i < saveBtnEl.length; i++) {
+    saveBtnEl[i].addEventListener('click', textBlockSave(i))
+};
+
+window.addEventListener('load', getStorage())
